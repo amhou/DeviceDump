91ad7f8e3463ea641081701f3191007fa2dee8ae
amhou <hou.andrew@gmail.com>
2012-01-19 17:29:00 -0800

    android version, network operator, device name

--- src/com/devicedump/DeviceDump.java
+++ src/com/devicedump/DeviceDump.java
@@ -2,6 +2,7 @@ package com.devicedump;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 import android.telephony.TelephonyManager;
@@ -17,6 +18,7 @@ public class DeviceDump extends Activity {
 
     public void dumpState(View button) {
         TelephonyManager tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
         String imei = tManager.getDeviceId();
         TextView imei_tv = (TextView) this.findViewById(R.id.imei_textview);
         imei_tv.setText("IMEI: " + imei);
@@ -29,8 +31,27 @@ public class DeviceDump extends Activity {
         TextView imsi_tv = (TextView) this.findViewById(R.id.imsi_textview);
         imsi_tv.setText("IMSI: " + imsi);
 
+        String manufacturer = Build.MANUFACTURER;
+        TextView manufacturer_tv = (TextView) this.findViewById(R.id.manufacturer_textview);
+        manufacturer_tv.setText("Manufacturer: " + manufacturer);
+
+        String device_name = Build.MODEL;
+        String device_code = Build.DEVICE;
+        TextView device_tv = (TextView) this.findViewById(R.id.device_textview);
+        device_tv.setText("Device: " + device_name + " / " + device_code);
+
+        String network_name = tManager.getNetworkOperatorName();
+        String network_id = tManager.getNetworkOperator();
+        TextView network_name_tv = (TextView) this.findViewById(R.id.network_name_textview);
+        network_name_tv.setText("Network: " + network_name + " (" + network_id + ")");
+
+        String sdk_version = Build.VERSION.RELEASE;
+        TextView sdk_tv = (TextView) this.findViewById(R.id.sdk_textview);
+        sdk_tv.setText("Android Version: " + sdk_version);
+
         String apn = Settings.ACTION_APN_SETTINGS;
         TextView apn_tv = (TextView) this.findViewById(R.id.apn_textview);
         apn_tv.setText("APN: " + apn);
+
     }
 }
