6120755f14da7e7ca862502d0c2e6b891cb3ed9b
Andrew Hou <hou.andrew@gmail.com>
2012-01-23 17:18:05 -0800

    ICCID, Roaming, Kernel, IMEI_SV

--- gen/com/devicedump/R.java
+++ gen/com/devicedump/R.java
@@ -14,17 +14,24 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ButtonRefresh=0x7f05000a;
+        public static final int ButtonRefresh=0x7f050011;
         public static final int ScrollView01=0x7f050000;
-        public static final int apn_textview=0x7f050009;
+        public static final int airplane_textview=0x7f05000f;
+        public static final int apn_textview=0x7f05000c;
         public static final int app_name_header=0x7f050001;
-        public static final int device_textview=0x7f050006;
-        public static final int imei_textview=0x7f050002;
-        public static final int imsi_textview=0x7f050004;
-        public static final int manufacturer_textview=0x7f050005;
-        public static final int mdn_textview=0x7f050003;
-        public static final int network_name_textview=0x7f050008;
-        public static final int sdk_textview=0x7f050007;
+        public static final int build_id_textview=0x7f050006;
+        public static final int device_textview=0x7f050005;
+        public static final int iccid_textview=0x7f050009;
+        public static final int imei_sv_textview=0x7f050008;
+        public static final int imei_textview=0x7f050007;
+        public static final int imsi_textview=0x7f050003;
+        public static final int is_roaming_textview=0x7f05000e;
+        public static final int kernel_version_textview=0x7f050010;
+        public static final int manufacturer_textview=0x7f050004;
+        public static final int mdn_textview=0x7f050002;
+        public static final int network_name_textview=0x7f05000b;
+        public static final int radio_version_textview=0x7f05000d;
+        public static final int sdk_textview=0x7f05000a;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/devicedump/DeviceDump.java
+++ src/com/devicedump/DeviceDump.java
@@ -8,22 +8,23 @@ import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 import android.telephony.TelephonyManager;
-import android.provider.Settings;
 import android.content.Context;
+import android.provider.Settings;
 
 public class DeviceDump extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        dumpState();
+    }
+    
+    public void refresh(View button) {
+        dumpState();
     }
 
-    public void dumpState(View button) {
+    public void dumpState() {
         TelephonyManager tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-
-        String imei = tManager.getDeviceId();
-        TextView imei_tv = (TextView) this.findViewById(R.id.imei_textview);
-        imei_tv.setText("IMEI: " + imei);
 
         String mdn = tManager.getLine1Number();
         TextView mdn_tv = (TextView) this.findViewById(R.id.mdn_textview);
@@ -42,14 +43,32 @@ public class DeviceDump extends Activity {
         TextView device_tv = (TextView) this.findViewById(R.id.device_textview);
         device_tv.setText("Device: " + device_name + " / " + device_code);
 
-        String network_name = tManager.getNetworkOperatorName();
-        String network_id = tManager.getNetworkOperator();
-        TextView network_name_tv = (TextView) this.findViewById(R.id.network_name_textview);
-        network_name_tv.setText("Network: " + network_name + " (" + network_id + ")");
+        String build_id = Build.ID;
+        String build_version_inc = Build.VERSION.INCREMENTAL;
+        TextView build_id_tv = (TextView) this.findViewById(R.id.build_id_textview);
+        build_id_tv.setText("Build ID: " + build_id + " / " + build_version_inc);
+
+        String imei = tManager.getDeviceId();
+        TextView imei_tv = (TextView) this.findViewById(R.id.imei_textview);
+        imei_tv.setText("IMEI/MEID: " + imei);
+        
+        
+        String imei_sv = tManager.getDeviceSoftwareVersion();
+        TextView imei_sv_tv = (TextView) this.findViewById(R.id.imei_sv_textview);
+        imei_sv_tv.setText("IMEI SV: " + imei_sv);
+        
+        String iccid = tManager.getSimSerialNumber();
+        TextView iccid_tv = (TextView) this.findViewById(R.id.iccid_textview);
+        iccid_tv.setText("ICCID: " + iccid);
 
         String sdk_version = Build.VERSION.RELEASE;
         TextView sdk_tv = (TextView) this.findViewById(R.id.sdk_textview);
         sdk_tv.setText("Android Version: " + sdk_version);
+
+        String network_name = tManager.getNetworkOperatorName();
+        String network_id = tManager.getNetworkOperator();
+        TextView network_name_tv = (TextView) this.findViewById(R.id.network_name_textview);
+        network_name_tv.setText("Network: " + network_name + " (" + network_id + ")");
 
         String apn_name = null;
         Cursor c = getContentResolver().query(Uri.parse("content://telephony/carriers/preferapn"), new String[]{"name","apn"},"current=1",null,null);
@@ -64,5 +83,21 @@ public class DeviceDump extends Activity {
         }
         TextView apn_tv = (TextView) this.findViewById(R.id.apn_textview);
         apn_tv.setText("APN: "  + apn_name);
+
+//        String radio_version = Build.getRadioVersion();
+//        TextView radio_version_tv = (TextView) this.findViewById(R.id.radio_version_textview);
+//        radio_version_tv.setText("Baseband: " + radio_version);
+
+        boolean is_roaming = tManager.isNetworkRoaming();
+        TextView is_roaming_tv = (TextView) this.findViewById(R.id.is_roaming_textview);
+        is_roaming_tv.setText("Roaming: " + is_roaming);
+        
+        String kernel_version = System.getProperty("os.version");
+        TextView kernel_version_tv = (TextView) this.findViewById(R.id.kernel_version_textview);
+        kernel_version_tv.setText("Kernel: " + kernel_version);
+        
+        String airplane = Settings.System.AIRPLANE_MODE_ON;
+        TextView airplane_tv = (TextView) this.findViewById(R.id.airplane_textview);
+        airplane_tv.setText("Airplane Mode: " + airplane);
     }
 }
